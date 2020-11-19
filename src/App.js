99ce52cad9ceb14039df71d9984a6670f0ce42c7
Kyanoush <kianoosh.sanatkar@yahoo.com>
2020-11-19 06:50:21 +0330

    add NavMenu & PagesURL to App

--- src/App.js
+++ src/App.js
@@ -1,19 +1,47 @@
 import React from 'react'
 import './App.style.scss';
+import { BrowserRouter } from "react-router-dom";
+import PagesURL from "./pages/pages.component";
+import NavMenu from './componenets/navmenu/navmenu.component';
+import Footer from './componenets/footer/footer.component'
 
 export default class App extends React.Component {
 
   constructor() {
     super();
     this.state = {
+      urls: {}
     }
+  }
+
+  componentDidMount() {
+    this.setState({ urls: this.getMenuLinks() });
   }
 
   render() {
     return (
       <div>
-        
+        <BrowserRouter>
+          <NavMenu urls={this.state.urls} />
+          <PagesURL />
+          <Footer />
+        </BrowserRouter>
       </div>
+    );
+  }
+
+  getMenuLinks() {
+    return (
+      {
+        "/": { text: "Home", parent: 'root', exact: true },
+
+        "/About": { text: "About", parent: 'root', exact: true },
+
+        "/Products": { text: "Gallery", parent: 'root', exact: true },
+        "/Products/sample product": { text: "Product", exact: true },
+
+      }
+
     );
   }
 }
