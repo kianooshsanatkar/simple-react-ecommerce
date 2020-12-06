82ac314c42e42ace693d52e8f36d630fc9d3db1d
Kyanoush <kianoosh.sanatkar@yahoo.com>
2020-12-06 06:57:17 +0330

    initialize and create NAV reducer, action, and mappers

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,8 +1,9 @@
 import {combineReducers} from 'redux';
 
 import userReducer from './user/user.reducer';
+import {navItemsReducer} from './navitems';
 
 export default combineReducers({
     user: userReducer,
-
+    nav: navItemsReducer
 });
