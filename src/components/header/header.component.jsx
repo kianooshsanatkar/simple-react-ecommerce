c77773c52bab989a0b7156fd51c4aae7ba62fc31
Kyanoush <kianoosh.sanatkar@yahoo.com>
2020-12-06 06:45:22 +0330

    Add Header component and move navMenu inside Header component

---
+++ src/components/header/header.component.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import NavMenu from '../navmenu/navmenu.component';
+
+
+class Header extends React.Component{
+
+    render(){
+        return (
+            <div>
+                <NavMenu />
+            </div>
+        );
+    }
+
+}
+
+
+export default Header;
