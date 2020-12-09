4b5286c4c783ebe21c5372675f6dea127988d413
Kyanoush <kianoosh.sanatkar@yahoo.com>
2020-12-09 08:07:44 +0330

    refactor: renaming

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Provider} from 'react-redux';
 
-import store from './redux/store';
 import App from './App';
+import store from "./redux/store";
+import { Provider } from "react-redux";
 
 
 ReactDOM.render(
-    <Provider store={store} >
+    <Provider store={store}>
         <App />
     </Provider>
     , document.getElementById('root'));
